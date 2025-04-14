a7af805f56d47aa8c5133fa17eb7e33a6dbb95e7
Janeway <janewaya@berkeley.cs.grinnell.edu>
2025-04-14 17:44:42 -0500

    Magic established

--- src/main/java/edu/grinnell/csc207/lootgenerator/LootGenerator.java
+++ src/main/java/edu/grinnell/csc207/lootgenerator/LootGenerator.java
@@ -1,5 +1,6 @@
 package edu.grinnell.csc207.lootgenerator;
 
+import edu.grinnell.csc207.lootgenerator.MagicOptions.Magic;
 import edu.grinnell.csc207.lootgenerator.Monsters.Monster;
 import java.io.FileNotFoundException;
 
@@ -12,12 +13,41 @@ public class LootGenerator {
         Monsters Pauls = new Monsters();
         Monster Paul = Pauls.getMonster();
         System.out.println(Paul.toStringGood());
+        
         TreasureClass Hoard = new TreasureClass();
         String Loot = Hoard.selectLoot(Paul.getMonTreasureClass());
+        
+        MagicOptions Abracadabra = new MagicOptions();
+        Magic Suffix = Abracadabra.getSuffix();
+        String sufStr = "";
+        int sufVal = 0;
+        String sufCode = "";
+        
+        String preStr = "";
+        int preVal = 0;
+        String preCode = "";
+        if(Suffix != null){
+            sufStr = Suffix.getName();
+            sufVal = Suffix.getMagVal();
+            sufCode = Suffix.getMagCode();
+        }
+        Magic Prefix = Abracadabra.getPrefix();
+        if(Prefix != null){
+            preStr = Prefix.getName();
+            preVal = Prefix.getMagVal();
+            preCode = Prefix.getMagCode();
+        }
         
         //System.out.println("Fighting " + Paul.toString() + "...");
         //System.out.println("You have slain " + Paul.toString() + "!");
         //System.out.println(Paul.toString() + " dropped:");
-        System.out.println("\n" + Loot);
+        System.out.println("\n" + preStr + " " + Loot +  " " + sufStr + "\n");
+        if(preVal != 0){
+            System.out.println(preCode + ": " + preVal);
+        }
+        if(sufVal != 0){
+            System.out.println(sufCode + ": " + sufVal);
+        }
+        
     }
 }
